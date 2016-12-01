a20b1f0d997f97b45142deba904f7c0dc1760f8b
iscma <iscma@DESKTOP-M2QN1NC>
2016-12-01 12:29:59 -0600

    Add project files.

---
+++ PalabrasPalindromos.WebApi/Controllers/PalindromosController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PalabrasPalindromos.WebApi.Controllers
+{
+    [RoutePrefix("api/palindromo")]
+    public class PalindromosController : ApiController
+    {
+        [Route("Palindromo")]
+        public bool EsPalindromo(string palabra)
+        {
+
+        }
+    }
+}
